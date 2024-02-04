fb34fbaaa0419d7c1e41c08a44ed0efa6be3f197
juanlus.dev <juanlus.dev@gmail.com>
2024-02-04 19:30:24 +0100

    feat: main services

---
+++ src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { MonstersModule } from './monsters/monsters.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return {
+          uri: configService.get('MONGODB_URI'),
+        };
+      },
+    }),
+    AuthModule,
+    MonstersModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+     // whitelist: true,
+      transform: true,
+    }),
+  );
+
+  const port = app.get(ConfigService).get('PORT');
+  await app.listen(Number(port || 3000));
+}
+bootstrap();
